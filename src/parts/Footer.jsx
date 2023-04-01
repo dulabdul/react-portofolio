9753a2ec7beb88d079870e1bc0ac147dd6a134c0
dulabdul <dul.abdoel21@gmail.com>
2023-04-01 11:43:25 +0700

    edit footer

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 export default function Footer() {
+  const d = new Date();
+  let year = d.getFullYear();
   return (
     <footer className='footer p-3'>
-      <p className='text-center'>
-        &copy;2022.Develop by abdulrahman <FaHeart />
+      <p className='text-center fw-light fs-6'>
+        &copy;2021 - {year} Develop by abdulrahman <FaHeart />
       </p>
     </footer>
   );
